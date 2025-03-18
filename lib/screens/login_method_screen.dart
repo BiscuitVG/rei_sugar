4d4c148069f558cbca5a6e570720a10b65cdb9d7
Biscuit <ixbiscuitmdxd952@gmail.com>
2025-03-18 16:12:25 +0530

    Updated Splash screen
    >Added empty screen for login method screen.
    >Updated Splash screen with navigation.

---
+++ lib/screens/login_method_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginMethodScreen extends StatelessWidget {
+  const LoginMethodScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
 
+import 'login_method_screen.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF0088FF),
-      body: Column(
-        children: [
-          //logo
+      backgroundColor: Color(0xFF0088ff),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Image.asset('lib/images/rei_ayanami.png',
+                height: 150,
+              ),
 
-          //app title
+              const SizedBox(height: 24),
 
-          //loading animation
-        ],
+              //title
+              Text(
+                'rei_sugar',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+
+              const SizedBox(height: 44),
+
+              //get started button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginMethodScreen(),
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'GetStarted',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
